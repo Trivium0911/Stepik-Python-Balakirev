6ba841cae22add52349b56078631e276b81a7bce
Trivium0911 <triv_1993@mail.ru>
2022-06-16 13:36:56 +0300

    added task 1.7_2

---
+++ 1.7_2.py
@@ -0,0 +1,120 @@
+"""
+                        1.7 Методы класса (classmethod) и статические методы (staticmethod) 2
+
+Подвиг 7. В программе объявлен следующий класс для работы с формами ввода логин/пароль:
+
+class FormLogin:
+    def __init__(self, lgn, psw):
+        self.login = lgn
+        self.password = psw
+
+    def render_template(self):
+        return "\n".join(['<form action="#">', self.login.get_html(), self.password.get_html(), '</form>'])
+Который предполагается использовать следующим образом:
+
+login = FormLogin(TextInput("Логин"), PasswordInput("Пароль"))
+html = login.render_template()
+Необходимо прописать классы TextInput и PasswordInput, объекты которых формируются командами:
+
+login = TextInput(name, size)
+psw = PasswordInput(name, size)
+В каждом объекте этих классов должны быть следующие локальные свойства:
+
+name - название для поля (сохраняет передаваемое имя, например, "Логин" или "Пароль");
+size - размер поля ввода (целое число, по умолчанию 10).
+
+Также классы TextInput и PasswordInput должны иметь метод:
+
+get_html(self) - возвращает сформированную HTML-строку в формате (1-я строка для класса TextInput ; 2-я - для класса PasswordInput):
+
+<p class='login'><имя поля>: <input type='text' size=<размер поля> />
+<p class='password'><имя поля>: <input type='text' size=<размер поля> />
+
+Например, для поля login:
+
+<p class='login'>Логин: <input type='text' size=10 />
+
+Также классы TextInput и PasswordInput должны иметь метод класса (@classmethod):
+
+check_name(cls, name) - для проверки корректности переданного имя поля (возвращает True/False) по следующим критериям:
+
+- длина имени не менее 3 символов и не более 50;
+- в именах могут использоваться только символы русского, английского алфавитов, цифры и пробелы
+
+Если проверка не проходит, то должно генерироваться исключение командой:
+
+raise ValueError('некорректное имя поля')
+Для проверки допустимых символов в каждом классе должен быть прописан атрибут CHARS_CORRECT:
+
+CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
+CHARS_CORRECT = CHARS + CHARS.upper() + digits
+По заданию нужно объявить только классы TextInput и PasswordInput с соответствующим функционалом. Более ничего.
+
+P. S. В данном задании получится дублирование кода в классах TextInput и PasswordInput. На данном этапе - это нормально.
+
+
+"""
+
+from string import ascii_lowercase, digits
+
+
+class TextInput:
+    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
+    CHARS_CORRECT = CHARS + CHARS.upper() + digits
+    nm = ''
+    sz = ''
+
+    @classmethod
+    def check_name(cls, name):
+        if 3 <= len(name) <= 50 and any([i in cls.CHARS_CORRECT for i in name]):
+            return True
+        else:
+            raise ValueError('некорректное имя поля')
+
+    def __init__(self, name, size=10):
+        self.name = name
+        self.size = size
+        self.check_name(name)
+
+    def get_html(self):
+        return f"<p class='login'>{self.name}: <input type='text' size={self.size} />"
+
+
+class PasswordInput:
+    CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
+    CHARS_CORRECT = CHARS + CHARS.upper() + digits
+
+
+    @classmethod
+    def check_name(cls, name):
+        if 3 <= len(name) <= 50 and all([i in cls.CHARS_CORRECT for i in name]) == True:
+            return True
+        else:
+            raise ValueError('некорректное имя поля')
+
+
+    def __init__(self, name, size=10):
+        self.name = name
+        self.size = size
+        self.check_name(name)
+
+
+    def get_html(self):
+        return f"<p class='password'>{self.name}: <input type='text' size={self.size} />"
+
+
+class FormLogin:
+    def __init__(self, lgn, psw):
+        self.login = lgn
+        self.password = psw
+
+
+    def render_template(self):
+        return "\n".join(['<form action="#">', self.login.get_html(), self.password.get_html(), '</form>'])
+
+
+# эти строчки не менять
+login = FormLogin(TextInput("45746rthrtjrytj6D"), PasswordInput("357468G665j"))
+html = login.render_template()
+TextInput.check_name('45746rthrtjrytj6D')
+print(PasswordInput.check_name('357468G665:;'))
