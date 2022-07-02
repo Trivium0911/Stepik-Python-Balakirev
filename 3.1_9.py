68503d06aae90226d95eef3708e09e6feef325d1
Trivium0911 <triv_1993@mail.ru>
2022-07-03 00:42:38 +0300

    fixed task 3.1_9

---
+++ 3.1_9.py
@@ -0,0 +1,88 @@
+"""
+                    3.1 Методы __setattr__, __getattribute__, __getattr__ и __delattr__ 9
+
+Подвиг 9. Объявите в программе класс Dimensions (габариты) с атрибутами:
+
+MIN_DIMENSION = 10
+MAX_DIMENSION = 1000
+
+Каждый объект класса Dimensions должен создаваться командой:
+
+d3 = Dimensions(a, b, c)   # a, b, c - габаритные размеры
+и содержать локальные атрибуты:
+
+__a, __b, __c - габаритные размеры (целые или вещественные числа).
+
+Для работы с этими локальными атрибутами в классе Dimensions следует прописать следующие объекты-свойства:
+
+a, b, c - для изменения и считывания соответствующих локальных атрибутов __a, __b, __c.
+
+При изменении значений __a, __b, __c следует проверять, что присваиваемое значение число в диапазоне [MIN_DIMENSION; MAX_DIMENSION]. Если это не так, то новое значение не присваивается (игнорируется).
+
+С помощью магических методов данного занятия запретить создание локальных атрибутов MIN_DIMENSION и MAX_DIMENSION в объектах класса Dimensions. При попытке это сделать генерировать исключение:
+
+raise AttributeError("Менять атрибуты MIN_DIMENSION и MAX_DIMENSION запрещено.")
+Пример использования класса  (эти строчки в программе писать не нужно):
+
+d = Dimensions(10.5, 20.1, 30)
+d.a = 8
+d.b = 15
+a, b, c = d.a, d.b, d.c  # a=10.5, b=15, c=30
+d.MAX_DIMENSION = 10  # исключение AttributeError
+P.S. На экран ничего выводить не нужно.
+
+"""
+
+
+class Dimensions:
+    MIN_DIMENSION = 10
+    MAX_DIMENSION = 1000
+
+    def __init__(self, a, b, c):
+        self.__a = self.__b = self.__c = None
+        self.a = a
+        self.b = b
+        self.c = c
+
+    @classmethod
+    def __check_val(cls, val):
+        return type(val) in (int, float) and cls.MIN_DIMENSION <= val <= cls.MAX_DIMENSION
+
+    @property
+    def a(self):
+        return self.__a
+
+    @a.setter
+    def a(self, val):
+        if self.__check_val(val):
+            self.__a = val
+
+    @property
+    def b(self):
+        return self.__b
+
+    @b.setter
+    def b(self, val):
+        if self.__check_val(val):
+            self.__b = val
+
+    @property
+    def c(self):
+        return self.__c
+
+    @c.setter
+    def c(self, val):
+        if self.__check_val(val):
+            self.__c = val
+
+    def __setattr__(self, key, value):
+        if key in ("MIN_DIMENSION", "MAX_DIMENSION"):
+            raise AttributeError("Менять атрибуты MIN_DIMENSION и MAX_DIMENSION запрещено.")
+        super().__setattr__(key, value)
+
+
+d = Dimensions(10.5, 20.1, 30)
+d.a = 8
+d.b = 15
+a, b, c = d.a, d.b, d.c  # a=10.5, b=15, c=30
+d.MAX_DIMENSION = 10  # исключение AttributeError
